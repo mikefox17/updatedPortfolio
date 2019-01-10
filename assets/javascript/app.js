b17f85261eb796337b9f5af8a7c2ca4413293da3
Michael Fox <mikefox17@gmail.com>
2019-01-10 17:20:09 -0500

    final

--- assets/javascript/app.js
+++
@@ -1,9 +0,0 @@
-
-//WORKING ON CLICK SUBMIT FUNCTION TO CONTAIN INPUTS FROM FORM AND FIRE OFF AN EMAIL
-
-$("submitBTN").click(function() {
-    var firstName = $("first_name").val();
-    var lastName = $("last_name").val();
-    var email = $("email").val();
-    var message = $("textarea2").val();
-}
