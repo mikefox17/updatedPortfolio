d803a0171fce002f7174d933f27450fb2e3771fb
Michael Fox <mikefox17@gmail.com>
2019-01-10 00:27:43 -0500

    just needs basic jquery

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+//WORKING ON CLICK SUBMIT FUNCTION TO CONTAIN INPUTS FROM FORM AND FIRE OFF AN EMAIL
+
+$("submitBTN").click(function() {
+    var firstName = $("first_name").val();
+    var lastName = $("last_name").val();
+    var email = $("email").val();
+    var message = $("textarea2").val();
+}
